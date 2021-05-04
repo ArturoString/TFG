2b4e08ae1a4f49d968de8b79f990b598a20d1bc2
ArturoString <arkituro300@gmail.com>
2021-05-04 12:45:23 +0200

    componente nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import {Routes} from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { BarranavegacionComponent } from './barranavegacion/barranavegacion.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FooterComponent
+    FooterComponent,
+    BarranavegacionComponent
   ],
   imports: [
     BrowserModule
