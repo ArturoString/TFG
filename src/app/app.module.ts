4fe570b42395bafac00c3085f3b5ed651e8da817
ArturoString <arkituro300@gmail.com>
2021-05-07 14:23:36 +0200

    firebase configurado mprimera parte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/*import {Routes} from '@angular/router';*/
+
+import { AngularFireModule } from '@angular/fire'; // Firebase config
+import { AngularFirestoreModule } from '@angular/fire/firestore'; // For Cloud Firestore
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-/*import {Routes} from '@angular/router';*/
 import { FooterComponent } from './footer/footer.component';
 import { BarranavegacionComponent } from './barranavegacion/barranavegacion.component';
 import { RegistroComponent } from './registro/registro.component';
-
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,9 @@ import { RegistroComponent } from './registro/registro.component';
     RegistroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig), // Import firebase
+    AngularFirestoreModule, // Import firestore
   ],
   providers: [],
   bootstrap: [AppComponent]
