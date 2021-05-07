297252fc3e872cae64dfffde45c4a443ddfdc667
ArturoString <arkituro300@gmail.com>
2021-05-07 13:47:16 +0200

    boostrap y frebase instalados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import {Routes} from '@angular/router';
+/*import {Routes} from '@angular/router';*/
 import { FooterComponent } from './footer/footer.component';
 import { BarranavegacionComponent } from './barranavegacion/barranavegacion.component';
 import { RegistroComponent } from './registro/registro.component';
--- src/app/barranavegacion/barranavegacion.component.ts
+++ src/app/barranavegacion/barranavegacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-barranavegacion',
@@ -12,4 +13,7 @@ export class BarranavegacionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  clickAddTodo(): void{
+    alert('hola!');
+  }
 }
