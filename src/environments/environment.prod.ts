a8f76e506f9a1ea5bc4e3737f2a0ed1ce35eac3d
ArturoString <arkituro300@gmail.com>
2021-05-07 14:00:42 +0200

    firebase configurado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+const  firebaseConfig = {
+  apiKey: 'AIzaSyAIfw66YcHIFyK6rHeC06ILtQIzgSplTpo',
+  authDomain: 'herbiabd.firebaseapp.com',
+  projectId: 'herbiabd',
+  storageBucket: 'herbiabd.appspot.com',
+  messagingSenderId: '909589153073',
+  appId: '1:909589153073:web:ae62532c9953621b11dc10',
+  measurementId: 'G-EYRVV0Z4H7'
+};
+
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,21 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const  firebaseConfig = {
+  apiKey: "AIzaSyAIfw66YcHIFyK6rHeC06ILtQIzgSplTpo",
+  authDomain: "herbiabd.firebaseapp.com",
+  projectId: "herbiabd",
+  storageBucket: "herbiabd.appspot.com",
+  messagingSenderId: "909589153073",
+  appId: "1:909589153073:web:ae62532c9953621b11dc10",
+  measurementId: "G-EYRVV0Z4H7"
+};
+
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig
 };
 
 /*
